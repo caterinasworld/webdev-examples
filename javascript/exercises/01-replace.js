7ac99db5a7c27ea922a939b285e401abb2dad9f4
Caterina Paun <caterina.paun@gmail.com>
2021-07-18 16:09:39 -0700

    Update javascript exercises and solutions

--- javascript/exercises/01-replace.js
+++ javascript/exercises/01-replace.js
@@ -8,7 +8,7 @@ const replaceItems = (input) => {
 };
 
 console.log(replaceItems('All that is gold does not glitter'));
-// expected: 'All that is shiny does not glitter'
+// expected: 'All that is SHINY does not glitter'
 console.log(replaceItems('Wisdom is better than silver or GOLD.'));
 // expected: 'Wisdom is better than silver or SHINY.'
 console.log(replaceItems('Not all those who Wander are lost'));
